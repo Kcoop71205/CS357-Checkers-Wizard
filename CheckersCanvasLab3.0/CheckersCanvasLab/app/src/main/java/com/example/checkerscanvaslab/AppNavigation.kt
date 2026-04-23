b280f63da6494edc68141382dd5efbf9b3a80911
Aiden Jefferson <aj298@humboldt.edu>
2026-04-23 09:35:13 -0700

    Add files via upload
    3.0

---
+++ CheckersCanvasLab3.0/CheckersCanvasLab/app/src/main/java/com/example/checkerscanvaslab/AppNavigation.kt
@@ -0,0 +1,40 @@
+package com.example.checkerscanvaslab
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.checkerscanvaslab.ui.theme.CheckersBoardView
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen(
+                onPlayClick = {
+                    navController.navigate("play")
+                }
+            )
+        }
+
+        composable("play") {
+            AndroidView(
+                factory = { context ->
+                    CheckersBoardView(context).apply {
+                        onHomeClick = {
+                            navController.navigate("home") {
+                                popUpTo("home") { inclusive = true }
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    }
+}
